03388f04bab441ac3d591b78a3f82577a4417cda
ariel.plasencia <arielplasencia00@gmail.com>
2022-02-17 19:08:47 -0500

    update main.py for presentation

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+import qdarkstyle
+
+from PyForm.present import Present
 from PyQt5.QtWidgets import QApplication
 from PyForm.main_window import MainWindow
 from matplotlib.pyplot import switch_backend 
 
-import qdarkstyle
-
 if __name__ == '__main__':
+    
     app = QApplication([])
+
+    present = Present()
+    present.exec()
+
     app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
     switch_backend('agg')
 
